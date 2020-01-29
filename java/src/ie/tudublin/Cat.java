b3dada2dd0b3d53f1cff12cd5b942cb71273f344
Thomas Coll <thomascoll@android-b78813e1bf082e2c.ict.ad.dit.ie>
2020-01-29 16:35:42 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -17,4 +17,21 @@ public class Cat extends Animal
     {
         System.out.println("Meow!");
     }
+
+    public void kill()
+    {
+    	this.setNumLives((getNumLives() - 1));
+    	if (this.getNumLives() > 0) 
+    	{
+    		System.out.println("Ouch!");
+    	}
+    	else if (this.getNumLives() == 0)
+    	{
+    		System.out.println("Dead");
+    	}
+    	else
+    	{
+    		System.out.println("Something went wrong :/");
+    	}
+    }
 }
