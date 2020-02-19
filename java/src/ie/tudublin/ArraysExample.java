1ccc602a8596e13c04327d5f55a593f8a05f77bc
Thomas Coll <thomascoll@shane-daniel.ict.ad.dit.ie>
2020-02-19 17:59:34 +0000

    lab4 done

--- java/src/ie/tudublin/ArraysExample.java
+++ java/src/ie/tudublin/ArraysExample.java
@@ -12,6 +12,13 @@ public class ArraysExample extends PApplet
 	float[] rainFall = {45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58};
 	String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
+	float originX = 50;
+	float originY = 450;
+	float yAxisX = originX;
+	float yAxisY = originX;
+	float xAxisX = originY;
+	float xAxisY = originY;
+
 	public void settings()
 	{
 		size(500, 500);
@@ -21,17 +28,17 @@ public class ArraysExample extends PApplet
 	{
 		for(int i = 0 ; i < rainFall.length; i ++)
 		{
-			println(months[i] + "\t" + rainFall[i]);
+			//println(months[i] + "\t" + rainFall[i]);
 		}
 
 		for(float f:rainFall)
 		{
-			println(f);
+			//println(f);
 		}
 
 		for(String s:months)
 		{
-			println(s);
+			//println(s);
 		}
 
 		int minIndex = 0;
@@ -42,7 +49,7 @@ public class ArraysExample extends PApplet
 				minIndex = i;
 			}
 		}
-		println(months[minIndex] + " had the minimum rainfall of " + rainFall[minIndex]);
+		//println(months[minIndex] + " had the minimum rainfall of " + rainFall[minIndex]);
 		
 		int maxIndex = 0;
 		for(int i = 1 ; i < rainFall.length ; i ++)
@@ -52,14 +59,14 @@ public class ArraysExample extends PApplet
 				maxIndex = i;
 			}
 		}
-		println(months[maxIndex] + " had the maximum rainfall of " + rainFall[maxIndex]);
+		//println(months[maxIndex] + " had the maximum rainfall of " + rainFall[maxIndex]);
 
-		// You can also calculate the minimum and max of an arry this way:
+		// You can also calculate the minimum and max of an array this way:
 		// Note the array is of type Float not float
 		// Float are objects float is a primitive type
 		Float[] floatArray = {10.0f, 5.0f, 20.0f};
 		float min = Collections.min(Arrays.asList(floatArray)); 
-        float max = Collections.max(Arrays.asList(floatArray));
+		float max = Collections.max(Arrays.asList(floatArray));
 	}
 
 	void drawBarChart()
@@ -76,6 +83,67 @@ public class ArraysExample extends PApplet
 		}
 	}
 
+	void drawTrendLine()
+	{
+		drawAxes();
+		textSize(12);
+		stroke(130, 255, 255);
+
+		float graphWidth = originY - originX;
+		float graphWidthGap = graphWidth / (months.length - 1);
+		float valuesX = originX;
+		float valueY1;
+		float valueY2;
+		float yAxisTop = 450;
+
+		for(int i = 0; i < (rainFall.length - 1); i++)
+		{
+			valueY1 = map(rainFall[i], 0, 150, yAxisTop, originX);
+			valueY2 = map(rainFall[i + 1], 0, 150, yAxisTop, originX);
+
+			line(valuesX, valueY1, (valuesX + graphWidthGap), valueY2);
+
+			valuesX += graphWidthGap;
+		}
+	}
+
+	void drawAxes() {
+		stroke(255, 0, 255);
+		int numYValues = 16;
+		int yValue = 0;
+
+		float graphWidth = originY - originX;
+		float graphHeight = graphWidth;
+		float graphWidthGap = graphWidth / (months.length - 1);
+		float graphHeightGap = graphHeight / ((float)numYValues - 1);
+		float yAxisValues = originX - 5;
+		float xAxisValues = originY + 5;
+		float monthsX = originX;
+		float monthsY = originY + 20;
+		float yValuesY = originY;
+		float yValuesX = originX - 20;
+
+		line(originX, originY, yAxisX, yAxisY);
+		line(originX, originY, xAxisX, xAxisY);
+
+		textAlign(CENTER);
+		for(int i = 0; i < months.length; i++)
+		{
+			text(months[i], monthsX, monthsY);
+			line(monthsX, originY, monthsX, xAxisValues);
+			monthsX += graphWidthGap;
+		}
+
+		textAlign(RIGHT);
+		for(int i = 0; i < numYValues; i++)
+		{
+			text(yValue, yValuesX, yValuesY);
+			line(originX, yValuesY, yAxisValues, yValuesY);
+			yValuesY -= graphHeightGap;
+			yValue += 10;
+		}
+	}
+
 	public void keyPressed()
 	{
 		if (key == ' ')
@@ -89,6 +157,7 @@ public class ArraysExample extends PApplet
 		background(0);		
 		colorMode(HSB);	
 
-		drawBarChart();
+		//drawBarChart();
+		drawTrendLine();
 	}
 }
