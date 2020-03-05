a127a3222a34f4ba8634e8bcc7a30c8583d0e6bf
Thomas Coll <thomascoll@soc-cys83z2-is.ict.ad.dit.ie>
2020-03-05 10:09:56 +0000

    SoundSynthesis

--- java/src/ie/tudublin/SoundSynthesis.java
+++ java/src/ie/tudublin/SoundSynthesis.java
@@ -11,6 +11,9 @@ public class SoundSynthesis extends PApplet
     Minim minim;
     AudioOutput out;
     Oscil wave;
+    Oscil wave2;
+    Oscil wave3;
+    Oscil wave4;
 
     public void settings()
     {
@@ -23,8 +26,19 @@ public class SoundSynthesis extends PApplet
         minim = new Minim(this);
 
         out = minim.getLineOut();
-        wave = new Oscil( 440, 0.5f, Waves.SQUARE );
+
+        //A
+        wave = new Oscil(440, 0.5f, Waves.SQUARE );
         wave.patch(out);
+        //F
+        wave2 = new Oscil( 349.23f, 0.5f, Waves.SQUARE );
+        //wave2.patch(out);
+        //E
+        wave3 = new Oscil( 329.63f, 0.5f, Waves.SQUARE );
+        //wave3.patch(out);
+        //D
+        wave4 = new Oscil(293.66f, 0.5f, Waves.SQUARE );
+        //wave4.patch(out);
     }
 
     public void keyPressed() {
@@ -41,10 +55,10 @@ public class SoundSynthesis extends PApplet
     {
         //wave.setFrequency(random(60, 2000));
         float n = noise(offset);
-        wave.setFrequency(
-            map(n, 0, 1, 200, 600)
-        );
-        offset += 0.01f;
+        // wave.setFrequency(
+        //     map(n, 0, 1, 200, 600)
+        // );
+        // offset += 0.01f;
 
         float cx = width / 2;
         float cy = height / 2;
@@ -54,11 +68,34 @@ public class SoundSynthesis extends PApplet
         noFill();
         ellipse(cx, cy, 300, 300);
         ellipse(cx, cy, 20, 20);
-        ellipse(cx - 100, cy - 50, 20, 20);
-        ellipse(cx + 100, cy - 50, 20, 20);
+        ellipse(cx - 100, cy - 50, 20, map(n, 0, 1, 0, 60));
+        ellipse(cx + 100, cy - 50, 20, map(n, 0, 1, 0, 60));
 
         ellipse(cx, cy + 100, 100,
             map(n, 0, 1, 0, 60)
         );
+
+        line(cx - 150, cy, cx - 150, map(n, 0, 1, 0, 150));
+        line(cx + 150, cy, cx + 150, map(n, 0, 1, 0, 150));
+
+        // if(frameCount % 60 == 0)
+        // {
+        //     wave.patch(out);
+        // }
+
+        if(frameCount % 60 == 0)
+        {
+            wave.setFrequency(349.23f);
+        }
+
+        if(frameCount % 60 == 20)
+        {
+            wave.setFrequency(329.63f);
+        }
+
+        if(frameCount % 60 == 40)
+        {
+            wave.setFrequency(293.66f);
+        }
     }
 }
