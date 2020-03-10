7320e3f634a0b8e5dab48d1686fbf7b2190616c5
Thomas Coll <thomascoll@Thomass-MacBook-Air.local>
2020-03-10 13:05:52 +0000

    previous lab test not finished

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -113,6 +113,24 @@ public class Star
         );
     }
 
+    /*Bryan's solution
+    public void render(PApplet pa)
+    {
+        float border = pa.width * 0.05f;
+        float x = PApplet.map(xG, -5, 5, border, pa.width - border);
+        float y = PApplet.map(yG, -5, 5, border, pa.height - border);
+
+        pa.stroke(255,255, 0);
+        pa.line(x, y - 5, x, y, + 5);
+        pa.line(x - 5, y, x + 5, y);
+        pa.stroke(255, 0, 0);
+        pa.ellipse(x, y, absMag, absMag);
+        pa.textAlign(PApplet.LEFT, PApplet.CENTER);
+        pa.fill(255);
+        pa.text(displayName, x + 50, y);
+    } 
+    */
+
 
     
 }
