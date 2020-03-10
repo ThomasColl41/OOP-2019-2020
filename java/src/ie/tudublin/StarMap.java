685e83735843fcbc01f57205f71bd5e5fe017b99
Thomas Coll <thomascoll@Thomass-MacBook-Air.local>
2020-03-10 13:23:24 +0000

    1

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -12,6 +12,32 @@ public class StarMap extends PApplet
     // Generic
     ArrayList<Star> stars = new ArrayList<Star>(); 
 
+    public void drawStars()
+    {
+        for(Star s: stars)
+        {
+            s.render(this);
+        }
+    }
+
+    public void drawGrid()
+    {
+        float border = width * 0.05f;
+
+        stroke(0, 0, 255);
+        textAlign(CENTER, CENTER);
+        for(int i = -5 ; i <= 5 ; i ++)
+        {
+            float x = map(i, -5, 5, border, width - border);
+            line(x, border, x, height - border);
+            line(border, x, width - border, x);    
+            
+            fill(255);
+            text(i, x, border / 2);
+            text(i, border / 2, x);
+        }
+    }
+
     public void settings()
     {
         size(800, 800);
@@ -42,78 +68,11 @@ public class StarMap extends PApplet
         }
     }
 
-    /*Bryan's Solution
-    public void drawStars()
-    {
-        for(Star s:stars())
-        {
-            s.render(this);
-        }
-    }
-    
-    public void drawGrid()
-    {
-        float border = width * 0.05f;
-
-        stroke(0, 0, 255);
-        textAlign(CENTER, CENTER);
-        for(int i = -5; i <= 5; i++)
-        {
-            float x = map(i, -5, 5, border, width - border);
-            line(x, border, x, height - border);
-            line(border, x, width - border, x);
-        }
-
-        fill(255);
-        text(i, x, border / 2);
-        text(i, border / 2, x);
-    } 
-    */
-
     public void draw()
     {
-        colorMode(HSB);
-        background(0);
-        textSize(14);
-        textAlign(CENTER, CENTER);
-        stroke(150, 255, 255);
-        float gridCorner = 50;
-		int x = -5;
-		for (int i = 0; i <= 10; i++)
-		{
-            line(
-                gridCorner, 
-                map(i, 0, 10, gridCorner, width - gridCorner),
-                width - gridCorner, 
-                map(i, 0, 10, gridCorner, width - gridCorner)
-            );
-
-            line(
-                map(i, 0, 10, gridCorner, width - gridCorner), 
-                gridCorner,
-                map(i, 0, 10, gridCorner, width - gridCorner), 
-                height - gridCorner
-            );
-
-            text(
-                x,
-                map(i, 0, 10, gridCorner, width - gridCorner),
-                gridCorner / 2
-            );
-
-            text(
-                x,
-                gridCorner / 2,
-                map(i, 0, 10, gridCorner, width - gridCorner)
-            );
-
-			x++;
-        }
-        
-        /*Bryan's Solution
         background(0);
         drawGrid();
-        */
+        drawStars();
     }
 
 }
